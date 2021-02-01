bf85cbd003f69d1c1c4417bb42ecfc5ec056d218
Xerasin <phoenixteam20@gmail.com>
2021-02-01 15:36:44 -0800

    Very talented

--- lua/autorun/sitanywhere.lua
+++
@@ -1,9 +0,0 @@
-if SERVER then
-    AddCSLuaFile("sitanywhere/client/sit.lua")
-    AddCSLuaFile("sitanywhere/helpers.lua")
-    include("sitanywhere/helpers.lua")
-    include("sitanywhere/server/sit.lua")
-else
-    include("sitanywhere/helpers.lua")
-    include("sitanywhere/client/sit.lua")
-end
